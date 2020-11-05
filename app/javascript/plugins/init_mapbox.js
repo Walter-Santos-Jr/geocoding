58e7288c52dd38fb507ad57f52f27898ed701c74
Walter Jr <wj@tomcix.com>
2020-11-04 23:36:20 -0300

    Fit map to markers

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,5 +1,11 @@
 import mapboxgl from 'mapbox-gl';
 
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+};
+
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
@@ -17,6 +23,7 @@ const initMapbox = () => {
           .addTo(map);
       });
 
+      fitMapToMarkers(map, markers);
   }
 };
 
